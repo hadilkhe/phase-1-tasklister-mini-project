eeee77a5544daf09cbbdae8779eddc904546b40c
hadilkhe <121692875+hadilkhe@users.noreply.github.com>
2023-10-01 14:17:24 +0100

    finished my asseignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskForm = document.getElementById("create-task-form");
+  const newTaskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent form submission and page reload
+
+    const taskText = newTaskInput.value;
+    if (taskText.trim() !== "") {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = taskText;
+
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete";
+      deleteButton.addEventListener("click", function () {
+        taskItem.remove(); // Remove the task when the "Delete" button is clicked
+      });
+
+      taskItem.appendChild(deleteButton);
+      taskList.appendChild(taskItem);
+
+      newTaskInput.value = ""; // Clear the input field
+    }
+  });
 });
+
